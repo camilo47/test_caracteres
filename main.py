2041fcea322f8bac0246db912c97ae274cf71f7a
camilo47 <camilo.forero.cortes@outlook.es>
2020-05-07 12:19:49 -0500

    initial commit
    primer commint archivo inicial para ejecución

---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'Camilo47'
+__doc__ = '''archivio pricnipal para realizar pruebas de envio 
+de carcateres de escape  y especiales para validar control de estos a servicios web'''
